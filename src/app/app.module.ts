400b7bb5070c3e01755b17eb5a5f7553c634a181
jcalvarez <jcalvarez@gmail.com>
2018-04-05 10:32:54 +0200

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,24 @@ import { NgModule } from '@angular/core';
 // Rutas
 import { APP_ROUTER } from './app.routes';
 
+// Módulos
+import { PagesModule } from './pages/pages.module';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { BreadcumbsComponent } from './shared/breadcumbs/breadcumbs.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTER
+    APP_ROUTER,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
